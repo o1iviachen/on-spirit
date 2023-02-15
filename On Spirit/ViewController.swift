68a04e7d9a69acd016df85092e6835de3a0388b6
olivia <olivia63chen@gmail.com>
2023-02-14 19:56:53 -0500

    Initial Commit

---
+++ On Spirit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On Spirit
+//
+//  Created by olivia chen on 2023-02-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'On Spirit' do
+  use_frameworks!
+
+  # Pods for On Spirit
+  pod 'FirebaseUI'
+
+end
